36bce176b14652c648e25c4282bb9144a4ea24da
Anton Ukhanev <xedin.unknown@gmail.com>
2021-03-05 11:13:06 +0100

    Extend polyfill interface

--- src/StringableInterface.php
+++ src/StringableInterface.php
@@ -2,17 +2,19 @@
 
 namespace Dhii\Util\String;
 
+use Stringable;
+
 /**
  * Something that can be cast to string.
  *
  * @since 0.1
  */
-interface StringableInterface
+interface StringableInterface extends Stringable
 {
     /**
      * This method will be ran when casting this object to string.
      *
-     * This method {@link http://stackoverflow.com/a/2429735/565229 cannot throw an exception},
+     * This method {@link http://stackoverflow.com/a/2429735/565229 cannot throw an exception until PHP 8},
      * but can use `trigger_error()`.
      *
      * @since 0.1
